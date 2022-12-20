91f9a21525625baffa2474843cd6ec8cf57991df
Elina Kashina <linalars47@gmail.com>
2022-12-20 23:58:26 +0300

    upd 5.1

--- pr5/5.1.py
+++ pr5/5.1.py
@@ -1,24 +1,14 @@
 def main():
-    list1 = input().split()
-    list2 = input().split()
-    ans1=[]
-    ans2=[]
-    ans3=[]
-    ans4=[]
-    for i in range(0, len(list1)):
-        for j in range(0, len(list2)):
-            if list1[i]==list2[j]:
-                ans1.append(list1[i])
-        if list1[i] not in list2:
-            ans3.append(list1[i])
-            ans2.append(list1[i])
-    for j in range(0, len(list2)):
-        if list2[j] not in list1:
-            ans2.append(list2[j])
-            ans4.append(list2[j])
-    print(len(ans1), ' :' , ans1)
-    print(len(ans2) , ' :' , ans2)
-    print(len(ans3) , ' :' , ans3)
-    print(len(ans4) , ' :' , ans4)
+    my_set1 = set(input().split())
+    my_set2 = set(input().split())
+
+    ans1 = my_set1.intersection(my_set2)
+    ans2 = my_set1.symmetric_difference(my_set2)
+    ans3 = my_set1.difference(my_set2)
+    ans4 = my_set2.difference(my_set1)
+    print(len(ans1),'элемента :', *ans1)
+    print(len(ans2), 'элементов :', *ans2)
+    print(len(ans3), 'элементов :', *ans3)
+    print(len(ans4), 'элементов :', *ans4)
 if __name__=="__main__":
     main()
