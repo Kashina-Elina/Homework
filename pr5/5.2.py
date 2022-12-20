91f77cdfe37dce7a352b83598f9d060eff2a1800
Elina Kashina <linalars47@gmail.com>
2022-12-20 23:22:35 +0300

    homework 5.2

--- pr5/5.2.py
+++ pr5/5.2.py
@@ -1,7 +1,11 @@
 from itertools import*
-s = input().split()
-list1 = []
-for i in permutations(s):
-    if list(i) not in list1:
-        list1.append(list(i))
-print(list1)
+def main():
+    s = input().split()
+    list1 = []
+    for i in permutations(s):
+        if list(i) not in list1:
+            list1.append(list(i))
+    print(list1)
+
+if __name__=="__main__":
+    main()
