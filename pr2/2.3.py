1b62dd0b2a8980aa5698a58fc23739756dce30cd
Elina Kashina <linalars47@gmail.com>
2023-01-14 15:34:55 +0300

    upd 2.3

--- pr2/2.3.py
+++ pr2/2.3.py
@@ -1,38 +1,51 @@
 def main():
     x = ' '.join(input().split())
-    open = ''
-    startchar = 0
+    n = len(x)
+    flag = False
     maxlen = ''
-    for i in range(len(x)):
-        if x[i] in '([{':
-            open+=x[i]
-        else:
-            if x[i]==')':
-                if open.endswith('('):
-                    open= open[:-1]
+    for k in range(n):
+        if flag:
+            break
+        new_x = x
+        for j in range(len(x)):
+            if flag:
+                break
+            open = ''
+            for i in range(len(x)):
+                if x[i] in '([{':
+                    open += x[i]
                 else:
-                    open = ''
-                    startchar=i+1
-            if x[i]==']':
-                if open.endswith('['):
-                    open= open[:-1]
-                else:
-                    open = ''
-                    startchar = i + 1
-            if x[i]=='}':
-                if open.endswith('{'):
-                    open= open[:-1]
-                else:
-                    open = ''
-                    startchar = i + 1
-        if len(x[startchar:i+1])>len(maxlen):
-            maxlen=x[startchar:i+1]
-            if len(maxlen)%2==1:
-                maxlen = maxlen[1:]
-    if startchar==0:
-        print('true')
-    else:
-        print('false', maxlen)
+                    if x[i] == ')':
+                        if open.endswith('('):
+                            open = open[:-1]
+                        else:
+                            break
+                    if x[i] == ']':
+                        if open.endswith('['):
+                            open = open[:-1]
+                        else:
+                            break
+                    if x[i] == '}':
+                        if open.endswith('{'):
+                            open = open[:-1]
+                        else:
+                            break
+            else:
+                if open == '' and len(x) == n:
+                    flag = True
+                if open == '':
+                    if len(x) > len(maxlen):
+                        maxlen = x
+            x = x[:-1]
+        x = new_x[1:]
 
-if __name__=="__main__":
+    if len(maxlen)==0:
+        print('False')
+    elif flag:
+        print('True')
+    else:
+        print(maxlen)
+
+
+if __name__ == "__main__":
     main()
