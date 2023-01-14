97a02cda5fc5e31c2a5a6af48121f66567c013d0
Elina Kashina <linalars47@gmail.com>
2023-01-14 19:35:11 +0300

    upd lab1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -6,8 +6,9 @@ def signs(numbers: list, count: int, current_sum: int, sign: list, need_sum: int
     return (signs(numbers[1:], count - 1, current_sum + numbers[0], sign + ['+'], need_sum) or
             signs(numbers[1:], count - 1, current_sum - numbers[0], sign + ['-'], need_sum))
 
+
 def main():
-    file = open('lab1.txt', 'r+', encoding='utf-8')
+    file = open('lab1.txt', 'r+', encoding='utf-8')  # открываем файл с числами
     c = [int(i) for i in file.read().split()]
     k = len(c)-1
     n = c[0]  # количество чисел
@@ -15,15 +16,15 @@ def main():
     c = c[1:k]  # сумма
     rez = ''
     sign = signs(c[1:], len(c)-1, c[0], [], s)
-    for i in range(len(c)):
-        c[i] = str(c[i])
-    for i in range(0,len(sign)):
-        rez += c[i] + sign[i]
-    rez+=c[-1]+'='+str(s)
-    if len(sign)>0:
+    for i in range(0,len(sign)):  # выводим в результат число и знак, следующий за ним
+        rez += str(c[i]) + sign[i]
+    rez += str(c[-1]) + '=' + str(s)  # добавляем последнее число, равно и искомую сумму
+    if len(sign) > 0:  # если список со знаками не пустой, то записываем в файл результат
         file.write('\n'+rez)
-    else:
+    else:  # иначе, записываем в файл 'no solution'
         file.write('\nno solution')
-    file.close()
-if __name__=="__main__":
+    file.close()  # закрываем файл
+
+
+if __name__ == "__main__":
     main()
