265b2a83d284755759e61f19db49f061fd48a07d
Elina Kashina <linalars47@gmail.com>
2023-01-15 22:54:25 +0300

    upd 7

--- pr5/5.2.py
+++ pr5/5.2.py
@@ -1,11 +1,11 @@
 from itertools import combinations
 def main():
     s = input().split()
-    output = list()
+    output = set()
     for i in range(1, len(s)+1):
-        l = list(combinations(s, i))
-        output+=l
-    print('Подмножества:',set(output))
+        l = set(combinations(s, i))
+        output.update(l)
+    print('Подмножества:',output)
     print('Количество подмножеств:',len(output))
 if __name__=="__main__":
     main()
