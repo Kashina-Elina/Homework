6be12942b8a621e6e576e83f897d996f0212fe12
Elina Kashina <linalars47@gmail.com>
2023-06-07 19:23:11 +0300

    курсовой проект с комментариями

--- 2_семестр/KP/kp.py
+++ 2_семестр/KP/kp.py
@@ -1,26 +1,21 @@
 from tkinter import *
 from tkinter import messagebox
-
 from ScrollableFrame import *
 import customtkinter
 import pygame as pg
 import sys
 
 
-class ExceptionN(Exception):
-    ...
-
-
-class ExceptionL(Exception):
-    ...
-
-
-class ExceptionK(Exception):
-    ...
-
-
 class StartWindow:
+    """Класс, создающий стартовое окно ввода данных"""
     def __init__(self, master: 'Tk'):
+        """
+        master - окно, на которое добавлется текст, поля ввода и кнопки
+        v1 - функция регистрирующая self.validateN как функцию, которая будет выполнена при завершении работы программы.
+        v2 - функция регистрирующая self.validateL как функцию, которая будет выполнена при завершении работы программы.
+        v2 - функция регистрирующая self.validateK как функцию, которая будет выполнена при завершении работы программы.
+        draw_button - кнопка, при нажатии на которую вызывается функция создания нового окна
+        """
         self.master = master
         v1 = master.register(self.validateN)
         v2 = master.register(self.validateL)
@@ -41,6 +36,7 @@ class StartWindow:
         self.draw_button.grid(row=3, column=0)
 
     def validateN(self, n):
+        """Метод, проверяющий является ли введённое значение числом и входит ли в диапазон [2, 20]"""
         try:
             if not n:
                 return True
@@ -52,6 +48,7 @@ class StartWindow:
             return False
 
     def validateL(self, l):
+        """Метод, проверяющий является ли введённое значение числом"""
         try:
             if not l:
                 return True
@@ -63,6 +60,7 @@ class StartWindow:
             return False
 
     def validateK(self, k):
+        """Метод, проверяющий является ли введённое значение числом"""
         try:
             if not k:
                 return True
@@ -71,21 +69,33 @@ class StartWindow:
             else:
                 return False
         except ValueError:
-            raise ExceptionK('на ввод должно поступать число')
+            return False
 
 
     def get_coords(self, coords):
+        """Метод, возвращающий введенные координаты в виде пары x, y"""
         self.coords = coords.split()
         self.x = self.coords[0]
         self.y = self.coords[1]
         return self.x, self.y
 
     def create_NewWindow(self):
+        """Метод, создающий новое окно"""
         NewWindow(int(self.entry_n.get()), int(self.entry_l.get()),int(self.entry_k.get()), self.master)
 
 
 class NewWindow:
+    """Класс, создающий новое окно для воода координат"""
     def __init__(self, n, l, k, master: 'Tk'):
+        """
+        LIGHT_GREEN - задаёт цвет фигуры на зеленый
+        n, l, k - данные введённые пользователем и отвечающие за размер доски, количество фигур, которые нужно
+        расставить, количество фигур, которые уже стоят на доске
+        list_figures - список, в котором хранятся экземпляры класса Figure
+        master - окно, на которое добавлется текст, поля ввода и кнопки
+        scrollable_frame - фрейм с прокруткой
+        draw_button - кнопка, при нажатии на которую рисуется доска с фигурами
+        """
         self.LIGHT_GREEN = (144, 238, 144)
         self.n = n
         self.l = l
@@ -108,6 +118,7 @@ class NewWindow:
         self.draw_button.grid(row=k+1, column=0)
 
     def validateJ(self, j):
+        """Метод, проверяющий являются ли два введенных значения числами"""
         try:
             flag = True
             k = j.split()
@@ -122,12 +133,18 @@ class NewWindow:
             return False
 
     def func_get_entry(self):
+        """Метод, добавляющий экземпляры класса Figure в список list_figures"""
         for i in self.list_for_i:
             j = Figure(i.get().split()[0], i.get().split()[1], self.LIGHT_GREEN)
             self.list_figures.append(j)
         return self.list_figures
 
     def create_Board(self):
+        """
+        Метод, создающий экземпляр класса Board, осуществляющий проверку заданных фигур на равенство или нахождение
+        под боем, а также проверку на отсутствие решений. В случае прохождении проверок вызывает класс DrawBoard,
+        который рисует доску с фигурами
+        """
         b = Board(self.n, self.l, self.k, self.func_get_entry())
         if len(self.list_figures) > 1:
             for i in self.list_figures:
@@ -149,12 +166,18 @@ class NewWindow:
 
 
 class Figure:
+    """Класс, создающий фигуру"""
     def __init__(self, x, y, color):
+        """
+        x, y - задают координаты x и y, по которым фигура будет размещена на доске
+        color - цвет фигуры
+        """
         self.x = int(x)
         self.y = int(y)
         self.color = color
 
     def possible(self, n):
+        """Метод, добавляющий в список possible_variants варианты возможных ходов фигур"""
         possible_variants = []
         new_y = self.y - 1
         for j in range(-2, 3, 2):
@@ -170,7 +193,14 @@ class Figure:
 
 
 class Board:
+    """Класс, создающий доску"""
     def __init__(self, n, l, k, list_figures):
+        """
+        n, l, k - данные введённые пользователем и отвечающие за размер доски, количество фигур, которые нужно
+        расставить, количество фигур, которые уже стоят на доске
+        CORAL - задаёт красный цвет
+        list_figures - список с координатами фигур, введённых пользователем
+        """
         self.CORAL = (255, 127, 80)
         self.n = n
         self.l = l
@@ -189,19 +219,22 @@ class Board:
             print(*i)
 
 
-    def create_board(self): #функция должна рисовать пустую доску
+    def create_board(self):
+        """Метод, создающий пустую доску"""
         for i in range(self.n):
             str_in_board = []
             for j in range(self.n):
                 str_in_board.append('0')
             self.chess_b.append(str_in_board)
 
-    def add_figure(self): #функция должна добавлять фигуры, заданные пользователем на доску
+    def add_figure(self):
+        """Метод, добавляющий фигуры, введеныые пользователем из списка list_figures на доску"""
         for i in self.list_figures:
             self.chess_b[i.x][i.y] = '#'
         return self.chess_b
 
-    def hit(self, figure, flag): # проверка, находится ли фигура под боем
+    def hit(self, figure, flag):
+        """Метод, осуществляющий проверку, находится ли фигура под боем"""
         for i in figure.possible(self.n):
             if flag:
                 self.chess_b[i[0]][i[1]] = '*'
@@ -214,6 +247,11 @@ class Board:
             return self.chess_b
 
     def add_new_figure(self, start, l):
+        """
+        Метод, добавляющий возможные варианты расстановки фигур
+        possible_variants - список с координатами, куда может походить фигура
+        solution_options - варинаты, куда можно поставить количество фигур, заданное как l
+        """
         if l == 0:
             self.count_line += 1
             variant = []
@@ -264,7 +302,21 @@ class Board:
 
 
 class DrawBoard:
+    """Класс, рисующий доску"""
     def __init__(self, k_coords, new_coords, hit_variants, n, l, k, master, b):
+        """
+        k_coords - координаты фигур, введенных пользователем
+        new_coords - координаты выставленных программой фигур(первый вариант расстановки)
+        hit_variants - возможные варианты, куда может походить фига
+        n, l, k - данные введённые пользователем и отвечающие за размер доски, количество фигур, которые нужно
+        расставить, количество фигур, которые уже стоят на доске
+        b - экземпляр класса Board
+        file_out - файл для вывода решений
+        RES - размер шахматной доски
+        RES1 - размер окна, на которое добавляется шахматная доска
+        size - размер каждой клетки доски
+        BLACK, STATE_BLUE, BLEDZOLOT - черный, синий и светло-желтый цвета
+        """
         self.file_out = open('output.txt', 'w+', encoding='utf-8')
         self.master = master
         self.hit_variants = hit_variants
@@ -290,9 +342,9 @@ class DrawBoard:
         self.create_pgBoard()
 
     def output_file(self):
+        """Метод, осуществляющий запись всех вариантов расстановки фигур в файл"""
         variant = self.b.solution_options
-        print(variant)
-        for k in variant:  # запись всех вариантов расстановки фигур в файл output.txt
+        for k in variant:
             for i in k:
                 m = [str(i.x), str(i.y)]
                 self.file_out.write(f'({",".join(m)}) ')
@@ -300,10 +352,14 @@ class DrawBoard:
         return
 
     def create_pgBoard(self):
-        startText = self.fontObj.render('Output', True, (0, 0, 0), None)
+        """
+        Метод, рисующий доску с помощью pygame, а также вызывающий метод output_file для записи в файл решений при
+        нажатии на кнопку Output
+        """
+        Text = self.fontObj.render('Output', True, (0, 0, 0), None)
         outputButton = 620, 280, 150, 40
         self.button = pg.draw.rect( self.sc, (255, 255, 255), outputButton)
-        self.sc.blit(startText, (630, 290))
+        self.sc.blit(Text, (630, 290))
         pg.display.update()
         board = pg.Surface(self.RES)
 
